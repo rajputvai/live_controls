a8aa6a85a03dfd89c1ddc6c3558644188c71adf5
Vaibhav Rajput <rajputvai@gmail.com>
2018-09-01 15:06:20 +0530

    feat(player): dynamically load player

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -14,29 +14,32 @@ const styles = {
   },
 };
 
-const Player = props => {
-  const { classes, id, url, width, height } = props;
-  const style = {
-    width: `${width}px !important`,
-    height: `${height}px !important`,
-  };
-  return (
-    <div
-      id={id}
-      className={classNames('vxgplayer', classes.root)}
-      url={url}
-      aspect-ratio="true"
-      mute="true"
-      latency="150"
-      autostart="true"
-      nmf-src="javascripts/vxgplayer/pnacl/Release/media_player.nmf"
-      nmf-path="vxgplayer/media_player.nmf"
-      width={`${width}px`}
-      height={`${height}px`}
-      style={style}
-    />
-  );
-};
+class Player extends Component {
+
+  componentDidMount() {
+    window.vxgplayer(this.element.id)
+  }
+
+  render() {
+    const { classes, id, url, width, height } = this.props;
+    return (
+      <div
+        ref={el => this.element = el}
+        id={id}
+        className={classNames('vxgplayer', classes.root)}
+        url={url}
+        aspect-ratio="true"
+        mute="true"
+        latency="150"
+        autostart="true"
+        nmf-src="javascripts/vxgplayer/pnacl/Release/media_player.nmf"
+        nmf-path="vxgplayer/media_player.nmf"
+        width={`${width}px`}
+        height={`${height}px`}
+      />
+    );
+  }
+}
 
 Player.propTypes = {
   classes: PropTypes.object.isRequired,
