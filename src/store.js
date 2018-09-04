d09d04be92698a656a37ccedce7f849f806488b5
varun-c-amagi <varun.c@amagi.com>
2018-09-04 12:21:03 +0530

    fix applying dev tools extension

--- src/store.js
+++ src/store.js
@@ -6,12 +6,10 @@ import epics from './epics';
 
 const epicMiddleware = createEpicMiddleware();
 
-export default createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(epicMiddleware)
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(epicMiddleware)));
+
+export default store;
 
 epicMiddleware.run(epics);
