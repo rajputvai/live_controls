432ab3c1a68bc8845ddf280af579c03e6decefe5
praveen001 <geeks4share@gmail.com>
2018-09-08 13:48:22 +0530

    fixed memory leak

--- src/components/ControlsBody/index.js
+++ src/components/ControlsBody/index.js
@@ -137,13 +137,18 @@ class ControlsBody extends Component {
   state = { tab: 0 };
 
   componentDidMount() {
-    setTimeout(
-      () =>
-        setInterval(() => {
+    this.timeout = setTimeout(() => {
+      this.interval = setInterval(() => {
+        if (window.inputPlayer) {
           this.setState({ inputSourcePTS: window.inputPlayer.getPTSVideo() });
-        }, 40),
-      5000
-    );
+        }
+      }, 40);
+    }, 5000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+    clearInterval(this.interval);
   }
 
   handleTabChange = (event, value) => this.setState({ tab: value });
